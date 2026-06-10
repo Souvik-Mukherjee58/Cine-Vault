3b6e71e7a4e77e810327b663b8e3bafc0c22d8d2
Souvik-Mukherjee58 <213668469+Souvik-Mukherjee58@users.noreply.github.com>
2026-06-10 21:07:11 +0530

    fix: rename to CineVault in navbar

--- CineVault/Client/src/components/Navbar.jsx
+++ CineVault/Client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import '../css/Navbar.css'
 function Navbar() {
     return <nav className="navbar">
         <div className="navbar-brand">
-            <Link to="/">Movie App</Link>
+            <Link to="/">Cine Vault</Link>
         </div>
         <div className="navbar-links">
             <Link to="/" className="nav-link">Home</Link>
